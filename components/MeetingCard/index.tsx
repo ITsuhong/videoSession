8abd72b1b8291ffa5bd5dfd21224c312954631cf
suhong <1844966932@qq.com>
2024-04-28 18:00:22 +0800

    uodate:ui

--- components/MeetingCard/index.tsx
+++ components/MeetingCard/index.tsx
@@ -1,6 +1,11 @@
+import Image from "next/image";
+
 const MeetingCard = () => {
     return (
-        <div>会议卡片</div>
+        <section className="p-4 rounded-xl bg-[#1c1f2e] text-white">
+            <Image src="/icons/upcoming.svg" alt="" width={15} height={15}></Image>
+            <h1 className="text-[15px]">Team Sync:Spring Planning & Updates</h1>
+        </section>
     )
 }
 
--- components/MeetingTypeList/index.tsx
+++ components/MeetingTypeList/index.tsx
@@ -1,30 +1,36 @@
 import Image from "next/image";
 import HomeCard from "./HomeCard";
+import MeetingCard from "@/components/MeetingCard";
 
 const MeetingTypeList = () => {
     return (
-        <section className="grid grid-cols-1 gap-5 mt-4 md:grid-cols-2 lg:grid-cols-4">
-            <HomeCard
-                img="/icons/add-meeting.svg"
-                title="新建会议"
-                description="Setup a new recording"
-                className="bg-[#ff742e]"
-            />
-            <HomeCard
-                img="/icons/join-meeting.svg"
-                title="加入会议"
-                description="via invitation link"
-                className="bg-[#0e78f9]"/>
-            <HomeCard
-                img="/icons/schedule.svg"
-                title="安排会议"
-                description="Plan your meeting"
-                className="bg-[#820ef8]"/>
-            <HomeCard
-                img="/icons/recordings.svg"
-                title="会议记录"
-                description="Meeting recordings"
-                className="bg-[#f9a90e]"/>
+        <section>
+            <div className="grid grid-cols-1 gap-5 mt-4 md:grid-cols-2 lg:grid-cols-4">
+                <HomeCard
+                    img="/icons/add-meeting.svg"
+                    title="新建会议"
+                    description="Setup a new recording"
+                    className="bg-[#ff742e]"
+                />
+                <HomeCard
+                    img="/icons/join-meeting.svg"
+                    title="加入会议"
+                    description="via invitation link"
+                    className="bg-[#0e78f9]"/>
+                <HomeCard
+                    img="/icons/schedule.svg"
+                    title="安排会议"
+                    description="Plan your meeting"
+                    className="bg-[#820ef8]"/>
+                <HomeCard
+                    img="/icons/recordings.svg"
+                    title="会议记录"
+                    description="Meeting recordings"
+                    className="bg-[#f9a90e]"/>
+            </div>
+            <div className="grid grid-cols-2 gap-5 ">
+                <MeetingCard/>
+            </div>
         </section>
     )
 
